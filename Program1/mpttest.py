192c986097471dcfc0ad6313314cc3da14c59d7f
Eric Hallen <e.j.s.hallen@student.rug.nl>
2026-02-04 13:18:39 +0100

    fixed plt import

--- Program1/mpttest.py
+++ Program1/mpttest.py
@@ -1,5 +1,5 @@
 import numpy as npy
-import matplotlib as plt
+import matplotlib.pyplot as plt
 
 #basic plotting example to make sure that it works, also to refresh my memory on python
 x = npy.linspace(0,100,50)
