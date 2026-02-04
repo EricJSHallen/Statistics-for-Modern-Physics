7404dbad07425a5b0dce37cb29d38852876b9b5f
Eric Hallen <e.j.s.hallen@student.rug.nl>
2026-02-04 13:10:19 +0100

    this is a test plt plot file

---
+++ Program1/mpttest.py
@@ -0,0 +1,12 @@
+import numpy as npy
+import matplotlib as plt
+
+#basic plotting example to make sure that it works, also to refresh my memory on python
+x = npy.linspace(0,100,50)
+y = x
+
+plt.plot(x,y)
+
+
+
+
