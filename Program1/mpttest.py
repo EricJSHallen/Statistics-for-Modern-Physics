764a7e928d6f5b5ffb0f5789631a17ef4c8e3682
Eric Hallen <e.j.s.hallen@student.rug.nl>
2026-02-04 17:09:21 +0100

    made comment for branched file

--- Program1/mpttest.py
+++ Program1/mpttest.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 #basic plotting example to make sure that it works, also to refresh my memory on python
 x = npy.linspace(0,100,50)
 y = x
-
+#I am adding a new comment here because this is a test file demonstrating branching.
 plt.plot(x,y)
 plt.show()
 
